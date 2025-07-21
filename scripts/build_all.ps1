6d181ea6ead700192f736720317744fb0e1d17a3
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-21 18:15:59 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
-  -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -Version "1.0.0" `
+  -OutputFilePrefix "Intrusion_1.0" `
+  -ModName "Intrusion 1.0" `
+  -ModFolder "intrusion" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/intrusion/Intrusion-1.0-Original.zip" `
+  -ModBaseFilesUrlHash "d00f25ebaf38242df85c301c2681a4bb5e66e7415ecd74d8a5783f1e4fdf4f93" `
+  -ModReadmePath "ol_readme.txt" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
